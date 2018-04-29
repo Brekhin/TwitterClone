e3ab2be2025e19a20de6bbb01fcaf32873fa95a7
nilke1995 <brekhin@bk.ru>
2018-04-29 11:54:25 +0300

    The forms login/logout was added

--- src/main/java/ru/sweater/config/MvcConfig.java
+++ src/main/java/ru/sweater/config/MvcConfig.java
@@ -1,11 +1,13 @@
 package ru.sweater.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * Created by Алексей on 29.04.2018.
  */
+@Configuration
 public class MvcConfig implements WebMvcConfigurer{
 
     public void addViewControllers(ViewControllerRegistry registry){

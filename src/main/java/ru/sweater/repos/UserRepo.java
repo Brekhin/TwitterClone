3b6e1af74d621d091254929c126cc1525ef074f1
nilke1995 <brekhin@bk.ru>
2018-05-15 06:40:42 +0300

    service for working with mail was configured

--- src/main/java/ru/sweater/repos/UserRepo.java
+++ src/main/java/ru/sweater/repos/UserRepo.java
@@ -8,4 +8,6 @@ import ru.sweater.domain.User;
  */
 public interface UserRepo  extends JpaRepository<User, Long>{
     User findByUsername(String username);
+
+    User findByActivationCode(String code);
 }
